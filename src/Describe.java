00309d80618294ea3cf6e4ea38fafe1967b8fa87
stuart <sballantyne@protonmail.com>
2019-04-03 10:39:20 +0100

    Rest call

--- src/Describe.java
+++ src/Describe.java
@@ -1,9 +1,8 @@
 // https://studres.cs.st-andrews.ac.uk/CS1003/Practicals/W09/
 // https://studres.cs.st-andrews.ac.uk/CS1003/Practicals/W09/W09.pdf
 
-
 public class Describe {
 	public static void main(String[] args) {
-		System.out.println("hello");
+
 	}
 }
---
+++ src/Rest.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Rest {
+	private boolean Verbose = true;
+
+	private String Exec(String str) throws IOException {
+		URL url = new URL(str);
+
+		HttpURLConnection conn = (HttpURLConnection)
+				url.openConnection();
+
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "*/*");
+		conn.setRequestProperty("User-Agent", "curl/7.37.0");
+
+		if (Verbose) {
+			int i = 1;
+
+			String header = conn.getHeaderField(0);
+			System.out.println(header);
+			System.out.println("---------------------");
+			while ((header = conn.getHeaderField(i)) != null) {
+				String key = conn.getHeaderFieldKey(i);
+				System.out.println((key == null ? "" : key + ": ") + header);
+
+				++i;
+			}
+
+			System.out.println("---------------------");
+		}
+
+		if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("HTTP error code: " + conn.getResponseCode());
+		}
+
+		// I cringe every time I have to instantiate a dozen objects just
+		// to read a string line by line
+		BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+		StringBuilder buffer = new StringBuilder();
+		String output;
+		while ((output = br.readLine()) != null) {
+			buffer.append(output);
+		}
+
+		conn.disconnect();
+
+		return buffer.toString();
+	}
+}
